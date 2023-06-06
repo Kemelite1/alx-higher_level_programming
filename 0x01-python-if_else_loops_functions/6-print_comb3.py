a5c51390f51a0ba973c8b09986f3099318331f0d
Kemelite1 <osikemelite42@gmail.com>
2023-06-06 14:37:48 -0700

    2552 2662 2772 2882 2992 3443 3553 3663 3773 3883 3993 4554 4664 4774 4884 4994 5665 5775 5885 5995 6776 6886 6996 7887 7997 8998 root@70ef0671706e:/alx-higher_level_programming/0x01-python-if_else_loops_functions# vi 6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for first_number in range(0, 10):
+    for second_number in range(first_number + 1, 10):
+        print("{:d}{:d}".format(first_number, second_number), end=" ")
+print()
