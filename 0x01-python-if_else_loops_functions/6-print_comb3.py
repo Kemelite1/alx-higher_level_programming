fae5b1158ea569a3fe018a2159daf14e5eaf7aa4
Kemelite1 <osikemelite42@gmail.com>
2023-06-06 15:26:52 -0700

    a function that checks for lowercase character

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for first_number in range(0, 10):
+for first_number in range(10):
     for second_number in range(first_number + 1, 10):
-        print("{:d}{:d}".format(first_number, second_number), end=" ")
+        print("{:d}{:d}, ".format(first_number, second_number), end="")
 print()
