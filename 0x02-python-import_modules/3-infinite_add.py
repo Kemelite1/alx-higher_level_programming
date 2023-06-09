0b50c989cea86019ec7f603f32f5a68836170e98
Kemelite1 <osikemelite42@gmail.com>
2023-06-08 17:49:07 -0700

    if __name__ == __main__:

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    total_args = 0
-    for i in range(len(sys.argv) - 1):
-        total_args += int(sys.argv[i + 1])
-        print("{}".format(total_args))
+    args = sys.argv[1:]
+    result = sum(int(arg) for arg in args)
+    print(result)
