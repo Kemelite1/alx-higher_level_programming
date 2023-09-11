5e54777cad25886625b541239f32b54872422278
Kemelite1 <osikemelite42@gmail.com>
2023-09-11 13:38:22 -0700

    fixed a bug

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const argument1 = process.argv[2];
 const number = parseInt(argument1);
-message = !isNaN(number) ? `My number: ${number}` : "Not a number";
+message = isNaN(number) ? `My number: ${number}` : "Not a number";
 console.log(message);
