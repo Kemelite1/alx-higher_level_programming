7bb6612b94dc71dff33449253a9917dd43a8f89c
Kemelite1 <osikemelite42@gmail.com>
2023-09-11 13:57:12 -0700

    a script that prints three lines  by using an array of string and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const threeLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < threeLines.length; i++){
-	console.log(threeLines[i];
+	process.stdout.write(threeLines[i] + '\n');
 }
