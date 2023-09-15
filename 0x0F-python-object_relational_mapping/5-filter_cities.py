6dd9a6197d691899176c01e1447fe8a4bf8ac54a
Kemelite1 <osikemelite42@gmail.com>
2023-09-15 16:25:35 -0700

    a script that takes in the name of a state as an argument and lists all cities of that state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -4,16 +4,22 @@ Takes in the name of a state
 as an argument and lists all cities of that
 state, using the database `hbtn_0e_4_usa`.
 """
+
 import MySQLdb
 import sys
+
+"""
+Accessing the database in order
+to get the cities.
+"""
+
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
-    cursor = db.cursor() # creates a cursor
-    cursor.execute("SELECT cities.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id
-                WHERE states.name=%s, (sys.argv[4],)") # fetch states sorted by states.id
-    states = cursor.fetchall() # fetches all rows
-    for row in states:
-        print(row)
-        cursor.close() # close all cursors
-        db.close() # close all database
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    mycursor = db.cursor()
+    mycursor.execute("""SELECT cities.name FROM cities INNER JOIN states ON states.id=cities.state_id WHERE states.name=%s""", (sys.argv[4],))
+    rows = mycursor.fetchall()
+    temp = list(row[0] for row in rows)
+    print(*temp, sep=", ")
+    mycursor.close()
+    db.close()
