81bc7f6dc39dc921e810b8f3bffc2b472fc6edb5
Kemelite1 <osikemelite42@gmail.com>
2023-09-15 15:15:00 -0700

    a script that lists all states from the database hbtn_0e_0_usa in ascending order

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     """
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     mycursor = db.cursor() # create a cursor
-    mycursor.execute("SELECT * FROM states")
+    mycursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = mycursor.fetchall()
     for row in rows:
         print(row)
