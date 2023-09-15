01600aae5a1d26a3e80265fc14c4e41850a3df79
Kemelite1 <osikemelite42@gmail.com>
2023-09-15 16:14:50 -0700

    a script that lists all cities from the database

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa"""
+"""  
+lists all cities from
+the database `hbtn_0e_4_usa`.
+"""
 import MySQLdb
 import sys
+
+"""
+Accessing the database in order
+to get the cities.
+"""
+
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306)
-    cursor = db.cursor() # creates a cursor
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id") # fetch states sorted by states.id
-    states = cursor.fetchall() # fetches all rows
-    for row in states:
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    mycursor = db.cursor()
+    mycursor.execute("""SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id=cities.state_id""")
+    rows = mycursor.fetchall()
+    for row in rows:
         print(row)
-        cursor.close() # close all cursors
-        db.close() # close all databases
+    mycursor.close()
+    db.close()
