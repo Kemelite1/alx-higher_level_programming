8750acf510965610404ef75479c9959042686380
Kemelite1 <osikemelite42@gmail.com>
2023-09-29 13:56:51 -0700

    a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import requests
-import sys
 
 """
 Takes in a letter, sends POST request to
 http://0.0.0.0:5000/search_user with the letter as 
 a parameter
 """
+import requests
+import sys
+
 
 if __name__ == "__main__":
     try:
