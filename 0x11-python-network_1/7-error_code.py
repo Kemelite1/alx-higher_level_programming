394807dbf01c979cff4cdcc63ae1a45612eb3a14
Kemelite1 <osikemelite42@gmail.com>
2023-09-29 13:53:45 -0700

    a Python script that takes in a URL, sends a request to the URL and displays the body of the response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import requests
-import sys
 
 """
 Takes in a URL, sends a request to the URL,
 and displays the body of the response
 """
+import requests
+import sys
+
 
 if __name__ == "__main__":
     response = requests.get(sys.argv[1])
