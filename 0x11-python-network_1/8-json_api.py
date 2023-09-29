1c7de0ffff34ac51b2cad3daad305338887cef24
Kemelite1 <osikemelite42@gmail.com>
2023-09-29 14:44:03 -0700

    JSON

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,17 +7,17 @@ a parameter
 """
 import requests
 import sys
-
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
-
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = r.json()
-        if response == {}:
-            print("No result")
+        param = {"q": sys.argv[1]}
+    except IndexError:
+        param = {"q": ""}
+    response = requests.post("http://0.0.0.0:5000/search_user", data=param)
+    res = response.json()
+    try:
+        if res:
+            print("[{}] {}".format(res.get("id"), res.get("name")))
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print("No result")
     except ValueError:
         print("Not a valid JSON")
