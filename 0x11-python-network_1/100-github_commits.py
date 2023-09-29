4c67db51ef9e70c2c1187c6a2c67861273b14943
Kemelite1 <osikemelite42@gmail.com>
2023-09-29 14:06:54 -0700

    A python script that lists the 10 most recent commits on a given GitHub repository

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""lists the 10 most recent commits on a given GitHub repository"""
+import sys
+
+import requests
+
+if __name__ == "__main__":
+    response = requests.get(
+        "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
+    )
+
+    res = response.json()
+
+    try:
+        for resp in res[:10]:
+            print(
+                "{}: {}".format(
+                    resp.get("sha"), resp.get("commit").get("author").get("name")
+                )
+            )
+    except IndexError:
+        pass
