d6e545bdd5700aeb20259ac5d71d578bb963f368
Kemelite1 <osikemelite42@gmail.com>
2023-09-29 13:51:09 -0700

    a Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import requests
-import sys
 
 """
 Takes in a URL and an email address, sends a POST request
 to the passed URL with the email as a parameter
 and displays the body of the response.
 """
+
+import requests
+import sys
+
 if __name__ == "__main__":
     param = {"email": sys.argv[2]}
     response = requests.post(sys.argv[1], data=param)
