490003c854c5b068f1271eaa863b4e03485e37db
Kemelite1 <osikemelite42@gmail.com>
2023-09-29 14:10:39 -0700

    lists 10 most recent Github repos

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 """lists the 10 most recent commits on a given GitHub repository"""
 import sys
-
+import requests
+import sys
 import requests
 
+
 if __name__ == "__main__":
-    response = requests.get(
-        "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
-    )
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    res = response.json()
-
+    r = requests.get(url)
+    commits = r.json()
     try:
-        for resp in res[:10]:
-            print(
-                "{}: {}".format(
-                    resp.get("sha"), resp.get("commit").get("author").get("name")
-                )
-            )
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
