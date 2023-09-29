9975e5e70dc0a8705a113cc8b494d194954279b6
Kemelite1 <osikemelite42@gmail.com>
2023-09-29 13:44:10 -0700

    a Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-from urllib import request, error
 
 """
 Takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8).
 """
+import sys
+from urllib import request, error
+
 
 if __name__ == "__main__":
 
