b970ecd10a0a4bb4a55baf15e66e80cab7769921
Kemelite1 <osikemelite42@gmail.com>
2023-10-02 13:31:36 -0700

    a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-# A Bash script that sends a request to a URL passed as an argument
-# and displays only the status code of the response.
-
-# Check if a URL argument is provided
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send the HTTP request and extract the status code using curl
-status_code=$(curl -sI "$1" | awk 'NR==1{print $2}')
-
-# Display the status code
-echo "Status Code: $status_code"
+# Write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -o /dev/null -s -w '%{http_code}' $1
