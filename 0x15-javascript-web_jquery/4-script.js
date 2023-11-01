b139ea4fa7f6eeeb0745651eb9d39f6f378d3250
kemelite1 <osikemelite42@gmail.com>
2023-11-01 22:14:14 +0100

    JQuery

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+    // Attach a click event handler to the div with ID "toggle_header"
+    $("#toggle_header").click(function() {
+      // Toggle the class of the <header> element between "red" and "green"
+      $("header").toggleClass("red green");
+    });
+  });
+  
